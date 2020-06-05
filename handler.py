fec204087781719dc910b65e0082581a5bd1dce7
Kirill <kirill.kolyaskin@new10.com>
2020-06-05 13:14:27 +0200

    Squashed commit of the following:
    commit 3bb87a645febdbe0584959a17da291c30a9043ae
    Author: Svetlana <gruzdevasvetlana@gmail.com>
    Date:   Mon Jun 1 14:07:01 2020 +0200
    
        awsDeploy
    
    commit ec97ad71cf1b6631ad1622524bf0a56e887b55de
    Merge: ffac7c2 3709a8f
    Author: Svetlana Gruzdeva <gruzdevasvetlana@gmail.com>
    Date:   Mon Jun 1 11:22:56 2020 +0000
    
        Merge branch 'refactor/sls' into 'GitLabAPI'
    
        Refactor/sls
    
        See merge request Svetlana.Gruzdeva/gitlab-contibution-analysis!2
    
    commit 3709a8fc1b080155badb169b158015f62baa3b7e
    Author: Svetlana Gruzdeva <gruzdevasvetlana@gmail.com>
    Date:   Mon Jun 1 11:22:56 2020 +0000
    
        Refactor/sls
    
    commit ffac7c2b6e6f6735eada9a555e55db673fee9d76
    Author: Svetlana <gruzdevasvetlana@gmail.com>
    Date:   Mon Jun 1 12:54:48 2020 +0200
    
        feat: get committer names
    
    commit a708488f4f80a52c963cf069c981d474c7ea43eb
    Author: Svetlana <gruzdevasvetlana@gmail.com>
    Date:   Sun May 31 18:33:30 2020 +0200
    
        feat: get all commit messages
    
    commit abba27364c3c1290e3d52f6bce72546a59ec1e15
    Author: Svetlana <gruzdevasvetlana@gmail.com>
    Date:   Sun May 31 17:38:05 2020 +0200
    
        Commint1

---
+++ handler.py
@@ -0,0 +1,45 @@
+import requests, os
+from pprint import pprint
+
+accessToken = os.environ.get('TOKEN')
+
+baseUrl = 'https://gitlab.com/api/v4'
+# TODO: encript before commit
+headers = {"Private-Token": f"{accessToken}"}
+userId = '5937077'
+
+# print(headers)
+
+def getUserProjects():
+    projectIdList = []
+    url = f'{baseUrl}/users/{userId}/projects'
+    r = requests.get(url, headers=headers)
+    userProjects = r.json()
+
+    for project in userProjects:
+        projectIdList.append(project['id'])
+
+    return(projectIdList)
+
+
+def getProjectCommits():
+    commitsList = []
+    for projectId in getUserProjects():
+        url = f"{baseUrl}/projects/{projectId}/repository/commits"
+        r = requests.get(url, headers=headers)
+        commitsPerProjectList = r.json()
+        commitsList.append(commitsPerProjectList)
+        print(projectId)
+        for commits in commitsList:
+            for i in range(len(commits)):
+                print(commits[i]['committer_name'])
+                # print(i)
+
+    return()
+
+
+def handler(event, context):
+    print(getProjectCommits())
+
+
+
