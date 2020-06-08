e039faa181f0eb9385db20bef96885d4ab8fde68
Svetlana <gruzdevasvetlana@gmail.com>
2020-06-08 11:54:51 +0200

    feat: count number of commits per person

--- handler.py
+++ handler.py
@@ -1,45 +1,49 @@
 import requests, os
 from pprint import pprint
+from collections import Counter
 
 accessToken = os.environ.get('TOKEN')
 
 baseUrl = 'https://gitlab.com/api/v4'
-# TODO: encript before commit
 headers = {"Private-Token": f"{accessToken}"}
 userId = '5937077'
 
-# print(headers)
-
 def getUserProjects():
-    projectIdList = []
+    projectsDict = {}
     url = f'{baseUrl}/users/{userId}/projects'
     r = requests.get(url, headers=headers)
     userProjects = r.json()
 
     for project in userProjects:
-        projectIdList.append(project['id'])
+        # projectIdList.append(project['id'])
+        projectsDict[project['id']] = project['name']
 
-    return(projectIdList)
+    return(projectsDict)
 
 
 def getProjectCommits():
     commitsList = []
-    for projectId in getUserProjects():
+    commiterNameList = []
+    uniqueCommiterNameList = []
+    
+    for projectId, projectName in getUserProjects().items():
         url = f"{baseUrl}/projects/{projectId}/repository/commits"
         r = requests.get(url, headers=headers)
         commitsPerProjectList = r.json()
         commitsList.append(commitsPerProjectList)
-        print(projectId)
+        print('Project ID: ', projectId,', Project Name: ', projectName)
+
         for commits in commitsList:
             for i in range(len(commits)):
-                print(commits[i]['committer_name'])
-                # print(i)
+                commiterNameList.append(commits[i]['committer_name'])
+
+        counter = Counter(commiterNameList)
+
+        print(counter.keys())
 
     return()
 
+print(getProjectCommits())
 
-def handler(event, context):
-    print(getProjectCommits())
-
-
-
+# def handler(event, context):
+#     print(getProjectCommits())
